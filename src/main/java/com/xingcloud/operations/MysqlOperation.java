2ac9d37811554e736cf573e6eb3ec68acfb995c5
griffinl <lq0987@126.com>
2015-01-09 19:32:37 +0800

    add mysql

--- src/main/java/com/xingcloud/operations/MysqlOperation.java
+++ src/main/java/com/xingcloud/operations/MysqlOperation.java
@@ -31,8 +31,8 @@ public class MysqlOperation {
     }
 
     public void clearOldData() throws SQLException {
-//        String[] pids = new String[]{"sof-wpm", "sof-zip", "sof-windowspm", "quick-start"};
-        String[] pids = new String[]{"sof-windowspm", "quick-start"};
+        String[] pids = new String[]{"sof-wpm", "sof-zip", "sof-windowspm", "quick-start"};
+//        String[] pids = new String[]{"sof-windowspm", "quick-start"};
 
         ExecutorService service = Executors.newFixedThreadPool(2);
         for(String pid : pids){
