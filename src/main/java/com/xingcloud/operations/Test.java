8c844553543afa6bf8bfe3e291754710935fb6e9
dannyhxwang <dannyhxwang@gmail.com>
2015-05-07 15:15:11 +0800

    changes

--- src/main/java/com/xingcloud/operations/Test.java
+++ src/main/java/com/xingcloud/operations/Test.java
@@ -1,6 +1,7 @@
 package com.xingcloud.operations;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.FsUrlStreamHandlerFactory;
 import org.apache.hadoop.fs.Path;
@@ -14,19 +15,23 @@ import java.net.URL;
  * Created by wanghaixing on 15-5-7.
  */
 public class Test {
-    public static String HDFS_PATH = "hdfs://ELEX-LA-WEB1:19000/user/hadoop/stream_log/pid/2015-05-06/22apple/stream_00000.log";
+    public static String FIX_PATH = "hdfs://ELEX-LA-WEB1:19000/user/hadoop/stream_log/pid/2015-05-06/22apple/";
 
     public static void main(String[] args) throws Exception {
         System.out.println("helllllllllll");
 
         Configuration conf = new Configuration();
-        FileSystem fs = FileSystem.get(URI.create(HDFS_PATH), conf);
-        InputStream in = null;
+        FileSystem fs = FileSystem.get(URI.create(FIX_PATH), conf);
+        for(FileStatus fileStatus: fs.listStatus(new Path(FIX_PATH))){
+            Path path = fileStatus.getPath();
+            System.out.println(path);
+        }
+        /*InputStream in = null;
         try {
             in = fs.open(new Path(HDFS_PATH));
             IOUtils.copyBytes(in, System.out, 4096, false);
         } finally {
             IOUtils.closeStream(in);
-        }
+        }*/
     }
 }
